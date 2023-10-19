c6ca0f9460eba0a2790a9dce757a66a4aacfd9e8
oishy404 <148414121+oishy404@users.noreply.github.com>
2023-10-19 10:49:42 +0600

    Add files via upload

---
+++ Notice/urlfile.py
@@ -0,0 +1,8 @@
+databs = {      "mobile" : "01512345678",
+    "password" : "123456"}
+url = "http://eidokan.com:3005/banani-club-member/web/v1/"
+urlapi = "http://eidokan.com:3005/bc-member/api/v1/"
+tokens = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMjAxZGIwMjkzZTJlMGVhZmQ1OTNlZiIsIm5hbWUiOiJKdXdlbCIsImVtYWlsIjoiVGVzdEBnbWFpbC5jb20iLCJtb2JpbGUiOiIwMTcwMDcwNDQzNiIsImRlcGFydG1lbnQiOiJBY2NvdW50cyIsInJvbGUiOiJNZW1iZXIiLCJzaWduZWRBdCI6IjIwMjMtMTAtMDRUMTE6NDY6MDcuNDg1WiIsImlhdCI6MTY5NjQxOTk2NywiZXhwIjoxNjk2NTA2MzY3fQ.bndRo2ZO3YhIbqpane3aOdb-lHt-E3ZrT2GZZ9pW3_s"
+# url = "https://trmisapi.inneedcloud.com/api/";
+token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MjI5NjUxNGE4MmZhNGY5Njk0NTBiNCIsIm5hbWUiOiJhbGkgRG9lIiwiZW1haWwiOiJ6aWJobG9AZG9tYWluLmNvbSIsIm1vYmlsZSI6IjAxNTEyMzQ1Njc4IiwiZGVwYXJ0bWVudCI6Ik5vdFNldCIsInJvbGUiOiJTdXBlcmFkbWluIiwic2lnbmVkQXQiOiIyMDIzLTEwLTA4VDExOjQ4OjI0LjQzN1oiLCJpYXQiOjE2OTY3NjU3MDQsImV4cCI6MTY5OTM1NzcwNH0.EqI8n-vBczH0T1qUBhBzFI55PqwCmwOefCJGcO-D5Ic"
+
